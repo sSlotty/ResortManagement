8c87be27305f107d7a62a459ab9dadce892d2e7d
Kittawat2543 <77712805+Kittawat2543@users.noreply.github.com>
2021-04-17 03:22:14 +0700

    Update Fragment

--- app/src/main/java/com/example/resortmanagement/Dashboard.kt
+++ app/src/main/java/com/example/resortmanagement/Dashboard.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.ImageView
 
 class Dashboard : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
