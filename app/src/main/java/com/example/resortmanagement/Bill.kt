f60c82b1e6b042f41afb6f43c88c69ae42756448
Kittawat2543 <77712805+Kittawat2543@users.noreply.github.com>
2021-04-16 22:51:10 +0700

    Add UI

---
+++ app/src/main/java/com/example/resortmanagement/Bill.kt
@@ -0,0 +1,11 @@
+package com.example.resortmanagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Bill : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bill)
+    }
+}
--- app/src/main/java/com/example/resortmanagement/Dashboard.kt
+++ app/src/main/java/com/example/resortmanagement/Dashboard.kt
@@ -1,11 +1,38 @@
 package com.example.resortmanagement
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        val booking = Intent(this,Booking::class.java)
+        val manageroom = Intent(this,ManageRoom::class.java)
+        val bill = Intent(this,Bill::class.java)
+        val employee = Intent(this,Employee::class.java)
+
+        var imageBooking = findViewById<ImageView>(R.id.btnBooking)
+        imageBooking.setOnClickListener{view->
+            startActivity(booking)
+        }
+
+        var imageManage = findViewById<ImageView>(R.id.btnManage)
+        imageManage.setOnClickListener{view->
+            startActivity(manageroom)
+        }
+
+        var imageBill = findViewById<ImageView>(R.id.btnBill)
+        imageBill.setOnClickListener{view->
+            startActivity(bill)
+        }
+
+        var imageEmployee = findViewById<ImageView>(R.id.btnEmployee)
+        imageEmployee.setOnClickListener{view->
+            startActivity(employee)
+        }
     }
 }
