8ac62a69a65db77035e0b7e6cd264e9c226bc59e
Oatwant <thanathip.oat@g.swu.ac.th>
2021-04-20 00:51:51 +0700

    update : Manage Room

--- app/src/main/java/com/example/resortmanagement/Room/ManageRoom.kt
+++ app/src/main/java/com/example/resortmanagement/Room/ManageRoom.kt
@@ -2,11 +2,22 @@ package com.example.resortmanagement.Room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.resortmanagement.R
 
 class ManageRoom : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manage_room)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frameLayoutManageroom,listRoomManagement.newInstance())
+            .commit()
+    }
+    fun chanageFragment(fragment:Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frameLayoutManageroom,fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
