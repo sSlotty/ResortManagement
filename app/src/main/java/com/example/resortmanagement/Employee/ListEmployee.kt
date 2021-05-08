2867a7c31ed60d871bbbf4cd2ea0f9f05d8da39a
Kittawat2543 <77712805+Kittawat2543@users.noreply.github.com>
2021-05-08 21:54:04 +0700

    list emp

--- app/src/main/java/com/example/resortmanagement/Employee/ListEmployee.kt
+++ app/src/main/java/com/example/resortmanagement/Employee/ListEmployee.kt
@@ -7,12 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.resortmanagement.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
@@ -32,10 +35,46 @@ private const val ARG_PARAM2 = "param2"
  */
 class ListEmployee : Fragment() {
 
-//    private var mRecyclerView:RecyclerView? = null
-//    private var mAdapter:RecyclerView.Adapter<RecyclerView.ViewHolder>? = null
-//    private var mLayoutManager:RecyclerView.LayoutManager? = null
+    private var mRecyclerView:RecyclerView? = null
+    private var mAdapter:RecyclerView.Adapter<RecyclerView.ViewHolder>? = null
+    private var mLayoutManager:RecyclerView.LayoutManager? = null
 
+    private class EventItemViewHolder(itemView:View):RecyclerView.ViewHolder(itemView)
+    {
+        val IdTxt:TextView = itemView.findViewById(R.id.idTxt)
+        val NameTxt:TextView = itemView.findViewById(R.id.nameTxt)
+        val JobpositionTxt:TextView = itemView.findViewById(R.id.jobpositionTxt)
+    }
+
+    private class EventListAdapter(var eventObjects:ArrayList<JSONObject>)
+        : RecyclerView.Adapter<RecyclerView.ViewHolder>()
+    {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val viewInflater:LayoutInflater = LayoutInflater.from(parent.context)
+            val entryView:View = viewInflater.inflate(R.layout.card_entry, parent, false)
+            val entryViewHolder:EventItemViewHolder = EventItemViewHolder(entryView)
+            return entryViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return eventObjects.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val eventObj = eventObjects.get(position)
+            val id = eventObj.getString("_id")
+            val name = eventObj.getString("name")
+            val jobposition = eventObj.getString("job_position")
+
+            if (holder is EventItemViewHolder)
+            {
+                val eventViewHolder:EventItemViewHolder = holder
+                eventViewHolder.IdTxt.text = id
+                eventViewHolder.NameTxt.text = name
+                eventViewHolder.JobpositionTxt.text = jobposition
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,11 +87,17 @@ class ListEmployee : Fragment() {
         // Inflate the layout for this fragment
         val v:View = inflater.inflate(R.layout.fragment_list_employee, container, false)
 
-        GlobalScope.launch(Dispatchers.Main) {
-            val result = httpGetEvents("http://localhost:5000/getuser")
-            Toast.makeText(activity, result, Toast.LENGTH_LONG).show()
-        }
+        mRecyclerView = v.findViewById(R.id.empRecyclerView)
+        mRecyclerView?.setHasFixedSize(true)
+        mLayoutManager = LinearLayoutManager(activity)
+        mRecyclerView?.layoutManager = mLayoutManager
 
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val result = httpGetEvents("https://resort-api-swu-comsci.herokuapp.com/getuser")
+
+            if (result!=null) parseJsonEvent(result)
+        }
         return v
     }
 
@@ -83,11 +128,19 @@ class ListEmployee : Fragment() {
         sb.toString()
     }
 
+    private var eventObjects = ArrayList<JSONObject>()
+    private fun parseJsonEvent(jsonStr:String)
+    {
+        eventObjects.clear()
 
-
-//    private class EventItemViewHolder(itemView:View):RecyclerView.ViewHolder(itemView)
-//    {
-//        val NameTxt:TextView = itemView.findViewById(R.id.NameTxt)
-//    }
+        val eventArray = JSONArray(jsonStr)
+        for (i in 0..eventArray.length()-1)
+        {
+            val eventObj = eventArray.getJSONObject(i)
+            eventObjects.add(eventObj)
+        }
+        mAdapter= EventListAdapter(eventObjects)
+        mRecyclerView?.adapter = mAdapter
+    }
 
 }
